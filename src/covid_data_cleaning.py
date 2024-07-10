9ea94bbe67ed9e51bac90fb90336e8c28bced745
marcelina.szczygiel <marcelina.szczygiel@midge-medical.com>
2024-07-10 19:52:44 +0200

    Extract EU data separately for each year

--- src/covid_data_cleaning.py
+++ src/covid_data_cleaning.py
@@ -19,7 +19,7 @@ input_data = data.copy()
 print(input_data.info()) # checks the data types
 
 # Cleaning the data (standardizing the format, removing rows with missing values)
-#input_data["Date_reported"] = pd.to_datetime(input_data["Date_reported"], format="%Y-%m-%d")
+
 input_data["Date_reported"] = pd.to_datetime(input_data["Date_reported"])
 
 
@@ -44,7 +44,7 @@ data_dict = {'2020': data_2020,'2021': data_2021, '2022': data_2022, '2023': dat
 
 timestamp = int(datetime.now().timestamp())
 
-#Create output directories for World data
+# Create output directories for World data:
 output_run_path = os.path.join(world_data_output_path, str(timestamp))
 os.makedirs(output_run_path, exist_ok=True)
 
@@ -53,7 +53,8 @@ for year, data in data_dict.items():
     data.to_csv(output_file_path, index=False)
     print(f"Saved data for year {year} to {output_run_path}")
 
-# Creating separate DataFrames for European Union data:
+### Extracting European Union data from World data into separate files:
+
 eu_countries = [
     "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", 
     "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", 
@@ -61,14 +62,32 @@ eu_countries = [
     "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"
 ]
 
-data_2020_EURO = input_data[input_data["Date_reported"].between("2020-01-01", "2020-12-31")]
-data_2020_EURO = data_2020_EURO[data_2020_EURO["WHO_region"]=="EURO"]
-data_2020_EU = data_2020_EURO[data_2020_EURO["Country"].isin(eu_countries)]
+# Creating separate DataFrames for each year for World data
 
-#Create output directory for European Union data:
+data_EURO = input_data[input_data["WHO_region"]=="EURO"]
+data_EU = data_EURO[data_EURO["Country"].isin(eu_countries)]
+
+currentdate = datetime.today().strftime('%Y-%m-%d')
+EU_data_2020 = data_EU[data_EU["Date_reported"].between("2020-01-01", "2020-12-31")]
+EU_data_2021 = data_EU[data_EU["Date_reported"].between("2021-01-01", "2021-12-31")]
+EU_data_2022 = data_EU[data_EU["Date_reported"].between("2022-01-01", "2022-12-31")]
+EU_data_2023 = data_EU[data_EU["Date_reported"].between("2023-01-01", "2023-12-31")]
+EU_data_2024 = data_EU[data_EU["Date_reported"].between("2024-01-01", currentdate)]
+
+
+# EU dictionary with years and data:
+EU_data_dict = {'2020': EU_data_2020,'2021': EU_data_2021, '2022': EU_data_2022, '2023': EU_data_2023, '2024': EU_data_2024}
+
+# Create output directories and save European Union data:
+
 output_EURO_run_path = os.path.join(EURO_data_output_path, str(timestamp))
 os.makedirs(output_EURO_run_path, exist_ok=True)
-output_EURO_file_path = os.path.join(output_EURO_run_path, f'data_EURO_{timestamp}.csv')
-data_2020_EU.to_csv(output_EURO_file_path, index=False)
+
+for year, data in EU_data_dict.items():
+    output_EURO_file_path = os.path.join(output_EURO_run_path, f'data_{year}_{timestamp}.csv')
+    data.to_csv(output_EURO_file_path, index=False)
+    print(f"Saved data for year {year} to {output_run_path}")
+
+
 
 
